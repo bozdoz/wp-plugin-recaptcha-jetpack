3fa5c26de07cf07c8e4b71e712b55e7529d9c9b4
bozdoz <howaboutben@gmail.com>
2017-07-21 01:33:03 -0300

    truly wtf

--- assets/js/invisible-recaptcha.js
+++ assets/js/invisible-recaptcha.js
@@ -12,23 +12,12 @@
 		$form = $this.closest('form.contact-form');
 
 		if (!$form.length) return;
-
-		// disable submit (for some reason)
-		$form.on('submit', function (e) {
-			if (e.isDefaultPrevented()) {
-				console.error('form validation failed');
-			} else {
-				e.preventDefault();
-				console.log('form success');
-				grecaptcha.execute();
-			}
-		})
-
 	});
 
 	window.bozdoz_jpr_onSubmit = function (token) {
 		// add token to form?
 		// submit form for some reason
+		alert(token);
 		$form.submit();
 	};
 
